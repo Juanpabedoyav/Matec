31d73e47c9b518677ed15bff8a5d2ee6dd2361ca
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-11-05 14:51:20 -0500

    📦 NEW:  Add test to validate search and create Order

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,13 +1,23 @@
 import { describe, test, expect } from 'vitest'
-import {render, screen} from '@testing-library/react'
+import {render,  screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from '../App'
 
 describe('<App />', () => {
-    test('should add items to the list', async() => {
-        render(<App />);
-        const allButton = screen.getAllByRole('button')
-        expect(allButton).toBeDefined();
+  render(<App />)
+  const user = userEvent.setup()
+
+    test('should show input search', async() => {
+        const input = screen.getByRole('textbox') as HTMLInputElement
+        expect(input).toBeDefined()
+        await user.type(input, 'iphone')
+        expect(input.value).not.toBe('')
       });
-        
+    test('should show the button create order', () =>{
+        const createOrder = screen.getByText(/Create Order/i)
+        expect(createOrder).toBeDefined()
+        createOrder.click()
+        screen.debug()
+    })
         
 })
