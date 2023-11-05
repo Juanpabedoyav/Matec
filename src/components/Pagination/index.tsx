ab775ee60619a130981c527e9ad5b04328d125df
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-11-04 23:41:26 -0500

    🐛 FIX: Render search results

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -10,7 +10,7 @@ interface PaginationProps {
 export default function  Pagination ({products, productsPerPage, backPage, nextPage}: PaginationProps) {
 return (
         <section className="pagination">
-          <p>Products: <strong>{productsPerPage().length}</strong> of <strong>{products.length}</strong></p>
+          <p>Products: <strong>{productsPerPage() ? productsPerPage().length : 0}</strong> of <strong>{products ? products.length : 0}</strong></p>
           <article className="pagination-buttons">
             <button className="preview preview-pagination" onClick={backPage}>Preview</button>
             <button className="next next-pagination" onClick={nextPage}>Next</button>
