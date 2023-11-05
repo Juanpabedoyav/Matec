54ca2b37dc976955518007ff624f856821740068
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-11-04 23:23:38 -0500

    👌 IMPROVE: Card product component

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,4 +1,5 @@
 import { Product } from "../../interfaces/products"
+import ProductCard from "../ProductCard/Index";
 
 interface ProductsListProps {
     productsPerPage: () => Product[];
@@ -11,25 +12,7 @@ return(
           {
             productsPerPage() && productsPerPage().map((item) => {
               return (
-                <article className="list" key={item.id}>
-                  <img height={200}  loading="lazy" src="https://flowbite.com/docs/images/products/apple-watch.png" alt={item.name} />
-                    <h3>{item.name}</h3>
-                  <section className="description-product">
-                    <p> <strong>${item.unit_price}</strong></p>
-                    <p>Category: <strong>{item.type}</strong></p>
-                  </section>
-                   {item.stock == 0 ? <p className="out-stock">🚨 Out of stock</p> : <p>Stock: <strong>{item.stock}</strong></p>} 
-                  <section>
-                    <form onSubmit={handlerSubmit(item)}>
-                     <input 
-                     name="quantity"
-                     type="number" 
-                     defaultValue={1}
-                     />
-                    <button type="submit">Add to Cart</button>
-                    </form>
-                  </section>
-                </article>
+                <ProductCard key={item.id} item={item} handlerSubmit={handlerSubmit}/>
               )
             }
             )
